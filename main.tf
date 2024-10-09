ebcccbefa052880b834d9779d38f3145a8cc8929
Pat Carey <pcarey@hashicorp.com>
2024-10-09 13:40:03 +1100

    update to data files.

--- main.tf
+++ main.tf
@@ -2,17 +2,14 @@
 locals {
   json_data = jsondecode(file("${path.module}/files/data.json"))
 
-  my_roles = [for custom_metadata in local.json_data.custom_metadata : custom_metadata.appid]
+  #my_roles = [for custom_metadata in local.json_data.custom_metadata : custom_metadata.appid]
 
   vault_policy_file = (templatefile("${path.module}/files/policy.tftpl", {
-    vault_roles = [local.my_roles[1]]
+    vault_roles = [local.json_data.custom_metadata.roles]
     }))
 
 }
 
-output "test" {
-    value = local.my_roles
-}
 
 output "template" {
     value = local.vault_policy_file
