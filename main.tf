7904a4c714c20879651c3a4a9a8ed26a7e445e87
Pat Carey <pcarey@hashicorp.com>
2024-10-09 13:45:18 +1100

    update

--- main.tf
+++ main.tf
@@ -2,28 +2,18 @@
 locals {
   json_data = jsondecode(file("${path.module}/files/data.json"))
 
-  #my_roles = [for custom_metadata in local.json_data.custom_metadata : custom_metadata.appid]
-
   vault_policy_file = (templatefile("${path.module}/files/policy.tftpl", {
-    vault_roles = [local.json_data.custom_metadata.roles]
+    vault_roles = toset(local.json_data.custom_metadata.roles[*])
     }))
-
 }
-
 
 output "template" {
     value = local.vault_policy_file
 }
-/*
+
 resource "vault_policy" "policy-per-role" {
-  for_each = local.inputhumanmap
-  name   = "policy-for-${each.key}"
-  policy = <<EOF
- path "secret/{{${each.value.role}}}" {
-    capabilities = ["create", "read", "update", "delete", "list" ]
- }
- EOF
+  name   = "policy-for-appid"
+  policy = local.vault_policy_file
 }
-*/
 
 
